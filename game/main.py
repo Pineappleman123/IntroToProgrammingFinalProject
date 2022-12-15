2c1d2f33a15bb662ce40730b24a6a9a5300f79ec
Pineappleman123 <86864397+Pineappleman123@users.noreply.github.com>
2022-12-15 09:17:34 -0800

    try except bug fix

--- game/main.py
+++ game/main.py
@@ -360,6 +360,7 @@ class Snake_Segment(Sprite):
                     # this statement checks if the snake segment in front of the current one has changed direction
                     # next_direction queues the direction of the snake segment in front of the current one but does not change direction just yet so that the current segment can catch up with the one in front of it and not cause gaps
                     # this block then allows change_direction to be true so that in the next update the segment can change direction
+                    # try except so that if there is indexing bug with rapid fire it will ignore it
                     try:
                         if self.direction != snake_segments[self.index - 1].direction:
                             self.next_direction = snake_segments[self.index - 1].direction
